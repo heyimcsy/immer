d0dc3bb1941065a192c73adeb3631c70ec8dd5d0
sychoi <cinnamon1995@naver.com>
2024-10-10 01:45:58 +0900

    make simple example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback, useRef, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({name: '', userName: ''})
+  const [data, setData] = useState({
+    array:[],
+    uselessValue: null
+  })
+
+  const onChange = useCallback((e) => {
+    const {name, value} = e.target
+    setForm({
+      ...form, [name]:[value]
+    })
+  },[form] )
+
+  const onSubmit = useCallback((e)=> {
+    e.preventDefault()
+    const info = {
+      id: nextId.current,
+      name: form.name,
+      userName: form.userName
+    }
+    setData({
+      ...data,
+      array: data.array.concat(info)
+    })
+
+    setForm({
+      name: '',
+      userName: ''
+    })
+
+    nextId.current += 1
+  },[data, form.name, form.userName] )
+
+  const onRemove = useCallback((id) => {
+    setData({
+      ...data, array: data.array.filter(info => info.id !== id)
+    })
+  },[data])
+
+  return(
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+        name='useName'
+        placeholder='아이디'
+        value={ form.userName}
+        onChange={onChange}
+        />
+        <input
+        name='name'
+        placeholder="이름"
+        value={form.name}
+        onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map(info => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.userName}({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
+
+
 }
 
 export default App;
