bd1d516463cced872ec4cdc02b6a26a745b3b2bd
sychoi <cinnamon1995@naver.com>
2024-10-12 23:58:10 +0900

    make parameter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Profile from './pages/Profile';
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="/profiles/:username" element={<Profile/>}/>
     </Routes>
   );
 }
